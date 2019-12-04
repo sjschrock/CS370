f2154ebabffe490c2f80ed2d4603e8119e32064f
sjschrock <samanthajschrock@gmail.com>
2019-12-03 20:13:51 -0700

    create qr

--- submission.py
+++ submission.py
@@ -1,19 +1,12 @@
 import sys
 import qrcode
-import qrcode.image.svg
-from PIL import Image
 
 uri = 'otpauth://totp/Example:alice@google.com?secret=JBSWY3DPEHPK3PXP&issuer=Example&period=30'
 
 if sys.argv[1] == '--generate-qr':
-    if method == 'basic':
-        factory = qrcode.image.svg.SvgImage
-    elif method == 'fragment':
-        factory = qrcode.image.svg.SvgGragmentImage
-    else:
-        factory = qrcode.image.svg.SvgPathImage
-    img = qrcode.make(uri, image_factory=factory)
-    Image.open(img).save('qr.svg')
+    img = qrcode.make(uri)
+    img.save('qr.svg')
+    
     print('made it')
     
 
